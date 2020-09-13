b64c5d6ac35135d0bc63e5f51e8ccfdc7e1af167
hdinh77 <heatherdinh11@gmail.com>
2020-09-13 14:49:27 -0700

    initial files

---
+++ tracker/src/main/java/heat/tracker/TrackerApplication.java
@@ -0,0 +1,13 @@
+package heat.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerApplication.class, args);
+	}
+
+}
